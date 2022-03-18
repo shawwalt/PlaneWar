e06e57c1058008add053d5786590fc4fbec7c020
shawwalt <664485929@qq.com>
2022-03-18 23:48:52 +0800

    move background

--- main.py
+++ main.py
@@ -33,27 +33,54 @@ class FrontEnd(QMainWindow, Ui_Form):
         self.setupUi(self)
         self.setWindowIcon(QIcon(':/images/windowicon.png'))
         self.config = load_json(':/settings/config.json')
+        self.width = self.config["SCENE_WIDTH"].toInt()
+        self.height = self.config["SCENE_HEIGHT"].toInt()
+        self.move_step = self.config["BACKGROUND_ROLL_STEP"].toInt()
+        self.flash_rate = self.config["FLASH_RATE"].toInt()
         self.init_scene()
         self.init_background()
+        self.start_game()
         self.show()
 
+    # 初始化方法区
     def init_background(self):
-        width = self.config["SCENE_WIDTH"].toInt()
-        height = self.config["SCENE_HEIGHT"].toInt()
         self.lb_background_1 = QLabel("", self)
         self.lb_background_2 = QLabel("", self)
         background_map = Map(':/images/background.png')
         self.lb_background_1.setPixmap(background_map)
         self.lb_background_2.setPixmap(background_map)
-        self.lb_background_1.setGeometry(0, -700, width, height)
-        self.lb_background_2.setGeometry(0, 0, width, height)
+        self.lb_background_1.setGeometry(0, -700, self.width, self.height)
+        self.lb_background_2.setGeometry(0, 0, self.width, self.height)
 
     def init_scene(self):
-        width = self.config["SCENE_WIDTH"].toInt()
-        height = self.config["SCENE_HEIGHT"].toInt()
         title = self.config["WIN_TITLE"].toString()
-        self.resize(width, height)
+        self.resize(self.width, self.height)
         self.setWindowTitle(title)
+
+    # 响应方法区
+    def start_game(self):
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.flash_screen)
+        self.timer.setInterval(self.flash_rate)
+        self.timer.start()
+
+    def flash_screen(self):
+        self.updata_position()
+
+    def updata_position(self):
+        y1 = self.lb_background_1.pos().y()
+        y2 = self.lb_background_2.pos().y()
+        if y1 <= 0:
+            y1 = y1 + self.move_step
+            self.lb_background_1.move(0, y1)
+        else:
+            self.lb_background_1.move(0, -self.height)
+
+        if y2 <= self.height:
+            y2 = y2 + self.move_step
+            self.lb_background_2.move(0, y2)
+        else:
+            self.lb_background_2.move(0, 0)
 
 
 class Map(QPixmap):
